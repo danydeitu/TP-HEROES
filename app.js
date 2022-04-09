06060e9cd4f963b3bae84227bda3b9ef37bcec42
Daniel <danydeitu18@gmail.com>
2022-04-09 16:34:03 -0300

    agrego img eniac

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app=express();
 const path=require('path');
 app.use(express.static('public'));
 const port=3030
-app.get('/',(req,res)=>
-    res.sendFile(path.join(__dirname,'views','index.html')));
 
+ app.get('/',(req,res)=>
+    res.sendFile(path.join(__dirname,'views','index.html')));   
     app.get('/babbage',(req,res)=>
     res.sendFile(path.join(__dirname,'views','babbage.html')));   
 
